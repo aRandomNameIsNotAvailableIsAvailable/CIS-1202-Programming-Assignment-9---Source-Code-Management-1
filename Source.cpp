aab9c01b0de5b095b33c31577aaec165092bfb7d
NPCPr <NPCPr@EIDOLON_LAPTOP>
2024-11-07 13:35:30 -0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,32 @@
+/*
+Name: Kyle Baker
+Date: 11/7/2024
+Project Name: CIS 1202 Programming Assignment #9 - Source Code Management 1
+Project Description: Write a program that displays the text "Hello, World", check it into a source code management system, modify the program, and check in the update.
+*/
+
+// libraries
+#include<iostream>
+
+// namespace
+using namespace std;
+
+	//											+++Start Main+++
+
+int main() {
+
+	// declare variables
+
+	cout << "Hello, World!" << endl;
+	
+	// edit these comments and check it within github version control 
+	// 123
+
+	//											+++END MAIN+++
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+
+}
+
